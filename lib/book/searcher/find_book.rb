01b94ff14655534499e48e7d8c7d83e7d38285d9
yuki-ohta0086 <yukiohta0823@gmail.com>
2021-01-02 21:15:58 +0900

    fix

--- lib/book/searcher/find_book.rb
+++ lib/book/searcher/find_book.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
+require 'net/http'
+require 'uri'
+require 'json'
 module Book
   module Searcher
     class Find_book
       GOOGLEAPI_URL = 'https://www.googleapis.com/books/v1/volumes?q='
 
       def get_book_json(params)
+        return if params.nil?
         uri = URI.parse(GOOGLEAPI_URL + params)
         resources = Net::HTTP.get(uri)
-        json_data = JSON.parse(resources)
-        puts json_data
+        puts resources
       end
     end
     class Error < StandardError; end
