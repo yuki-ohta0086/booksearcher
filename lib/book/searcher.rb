cf77a6f588401584a22341e51d5860e4dadd3090
yuki-ohta0086 <yukiohta0823@gmail.com>
2021-01-02 15:10:29 +0900

    作成

---
+++ lib/book/searcher.rb
@@ -0,0 +1,8 @@
+require "book/searcher/version"
+
+module Book
+  module Searcher
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/book/searcher/version.rb
@@ -0,0 +1,5 @@
+module Book
+  module Searcher
+    VERSION = "0.1.0"
+  end
+end
